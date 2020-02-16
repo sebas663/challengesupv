f8536500475b35efb9e6f5aceb82e2626d273121
sebas663 <sebastian_escuderoh@hotmail.com>
2020-02-16 17:20:10 -0300

    Se agrega Docker con configuracion productiva

--- app/src/main/java/com/supv/challenge/config/DatabaseConfiguration.java
+++ app/src/main/java/com/supv/challenge/config/DatabaseConfiguration.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
